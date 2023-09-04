9637871af64d2490073a9f1b0e325b6fdee6abef
Miguel Perez <miguel.p.donate@gmail.com>
2023-09-04 21:51:51 +0200

    initial

---
+++ python/baseball_stats.py
@@ -0,0 +1,15 @@
+# Baseball statistics challenge
+# 1. Calculate batting average of player using a function
+# 2. Calculate the ERA of a pitcher
+#    
+
+def batting_avg():
+    # hits/at bats
+    pass
+
+# enter the number of hits
+# enter at bats
+# display the average
+
+
+
